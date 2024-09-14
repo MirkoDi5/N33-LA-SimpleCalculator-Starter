4d448908ac0d8767c7e9a4a2f12dd7a93e34d6f3
MirkoDi5 <mirtheboss10@outlook.com>
2024-09-13 22:59:07 -0400

    Update InputConverterTest.cs

--- SimpleCalculator.Test.Unit/InputConverterTest.cs
+++ SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -4,47 +4,90 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SimpleCalculator.Test.Unit
 {
     [TestClass]
-    public class InputConverterTest
+    public class CalculatorEngineTest
     {
-        private readonly InputConverter _inputConverter = new InputConverter();
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
 
         [TestMethod]
-        public void ConvertsValidStringInputIntoDouble()
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
         {
-            string inputNumber = "5";
-            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
-            Assert.AreEqual(5, convertedNumber);
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            Assert.AreEqual(3, result);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void FailsToConvertsInvalidStringInputIntoDouble()
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
         {
-            string inputNumber = "*";
-            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
-            Assert.AreEqual(2, convertedNumber);
-        }
-    }
-
-    public class InputConverter
-    {
-        public InputConverter()
-        {
-
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+            Assert.AreEqual(3, result);
         }
 
-        public double ConvertInputToNumeric(string input)
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResult()
         {
-
-            double result;
-            if (double.TryParse(input, out result))
-            {
-                return result;
-            }
-            else
-            {
-                throw new ArgumentException("Invalid input");
-            }
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("subtract", number1, number2);
+            Assert.AreEqual(2, result);
         }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("-", number1, number2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResult()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("multiply", number1, number2);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("*", number1, number2);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResult()
+        {
+            int number1 = 10;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("divide", number1, number2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 10;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("/", number1, number2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void RaisesNumberToPowerAndReturnsValidResult()
+        {
+            int number1 = 2;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("power", number1, number2);
+            Assert.AreEqual(8, result);
+        }
+
     }
 }
