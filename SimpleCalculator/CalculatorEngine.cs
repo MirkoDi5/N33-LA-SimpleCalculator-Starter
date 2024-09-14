a776ced28af11ee0f4e550f04988576258f37006
MirkoDi5 <mirtheboss10@outlook.com>
2024-09-13 22:57:57 -0400

    Update CalculatorEngine.cs

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -1,15 +1,48 @@
-﻿using System;
+using System;
 
 namespace SimpleCalculator
 {
     public class CalculatorEngine
     {
-        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
 
-            if (argOperation == "+" || argOperation.ToLower() == "add") {
-                result = argFirstNumber + argSecondNumber;
+            switch (argOperation.ToLower())
+            {
+                case "+":
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "-":
+                case "subtract":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "*":
+                case "multiply":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "/":
+                case "divide":
+                    if (argFirstNumber == 0)
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
+                    else if (argSecondNumber == 0)
+                    {
+
+                        Console.WriteLine("Cannot divide this is equal to infinity");
+
+                    }
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                case "power":
+                case "pow":
+                case "^":
+                    result = Math.Pow(argFirstNumber, argSecondNumber);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid operation");
             }
 
             return result;
