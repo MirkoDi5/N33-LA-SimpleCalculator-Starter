0fb676026cae0c7cc752cf9fa8eec0544a5bfd33
MirkoDi5 <mirtheboss10@outlook.com>
2024-09-13 22:59:56 -0400

    Update CalculatorEngineTest.cs

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SimpleCalculator.Test.Unit
@@ -9,25 +9,85 @@ namespace SimpleCalculator.Test.Unit
         private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
 
         [TestMethod]
-        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
         {
-
             int number1 = 1;
             int number2 = 2;
             double result = _calculatorEngine.Calculate("add", number1, number2);
             Assert.AreEqual(3, result);
-
         }
 
         [TestMethod]
-        public void AddsTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
         {
-
             int number1 = 1;
             int number2 = 2;
             double result = _calculatorEngine.Calculate("+", number1, number2);
             Assert.AreEqual(3, result);
-
         }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResult()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("subtract", number1, number2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("-", number1, number2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResult()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("multiply", number1, number2);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 5;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("*", number1, number2);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResult()
+        {
+            int number1 = 10;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("divide", number1, number2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 10;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("/", number1, number2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void RaisesNumberToPowerAndReturnsValidResult()
+        {
+            int number1 = 2;
+            int number2 = 3;
+            double result = _calculatorEngine.Calculate("power", number1, number2);
+            Assert.AreEqual(8, result);
+        }
+
     }
 }
